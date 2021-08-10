e86fd5c8bffdfc528da1b191c9a7e930dab59cff
Oblosys <martijn@oblomov.com>
2021-08-11 01:44:35 +0200

    DEV-3655 Support test queries for multiple schemas

--- src/test/queries/agencyMember/agencyMemberTestQuery.ts
+++ src/test/queries/agencyMember/agencyMemberTestQuery.ts
@@ -3,13 +3,22 @@ import gql from "graphql-tag";
 
 type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 
-export type Scalars = {
+type Scalars = {
     ID: string;
     String: string;
     Boolean: boolean;
     Int: number;
     Float: number;
-}
+};
+
+type AgencyMember = {
+    id: string;
+    firstName: string;
+};
+
+type Agency = {
+    website: string;
+};
 
 const AgencyMemberGraphQL = {
     query<Args, Res>(_conn: any, _gqlDoc: graphql.DocumentNode, _args: Args): Res {
@@ -24,14 +33,12 @@ export const test = async () =>
         Exact<{
             name: Scalars["String"];
         }>,
-        {
-            __typename?: "Query";
-            agencyMembers: Array<{
-                __typename?: "AgencyMember";
-                id: any;
-                firstName: string;
-                agency: { __typename?: "Agency"; website: string };
-            }>;
+        { __typename?: "Query" } & {
+            agencyMembers: Array<
+                { __typename?: "AgencyMember" } & Pick<AgencyMember, "id" | "firstName"> & {
+                        agency: { __typename?: "Agency" } & Pick<Agency, "website">;
+                    }
+            >;
         }
     >(
         conn,
