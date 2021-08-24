88e0c0ba6e260c546987fb05523ea3508fb0c9fe
Martijn Schrage <martijn@oblomov.com>
2021-08-24 13:59:27 +0200

    DEV-3655 Add support for unions, interfaces, & inline fragments

--- src/codeGenerator.ts
+++ src/codeGenerator.ts
@@ -40,6 +40,31 @@ const typeFromAst = (
     ? graphql.typeFromAST(schema, typeNode)
     : graphql.typeFromAST(schema, typeNode);
 
+type PropertyFields = ReadonlyArray<{ name: string; type: string }>;
+
+type UnionOfFields = ReadonlyArray<{
+  propertyFields: PropertyFields;
+  condition?: graphql.GraphQLNamedType;
+}>;
+
+const groupByType = (unionOfFields: UnionOfFields): ReadonlyArray<PropertyFields> => {
+  const groupedFields: Record<string, PropertyFields> = {};
+  for (const { propertyFields, condition } of unionOfFields) {
+    const typeName = condition?.name ?? ""; // may seem odd, but '' is a valid object key (and cannot be a type name)
+    groupedFields[typeName] = [...(groupedFields[typeName] ?? []), ...propertyFields];
+  }
+  return Object.values(groupedFields);
+};
+
+const emitObjectType = (unionOfFields: UnionOfFields): string => {
+  const fields = groupByType(unionOfFields);
+  return fields
+    .map(
+      (fieldList) => "{ " + fieldList.map(({ name, type }) => `${name}: ${type}`).join(", ") + " }",
+    )
+    .join(" | ");
+};
+
 // ArgumentsType generation:
 
 const genArgumentsType_OperationDefinition = (
@@ -82,53 +107,136 @@ const genResultType_OperationDefinition = (
   operationDef: graphql.OperationDefinitionNode,
 ): string => {
   const selectionSet = operationDef.selectionSet;
-  const fieldPropertyListTypes = selectionSet.selections
-    .map(genResultType_Selection(schema, graphql.getOperationRootType(schema, operationDef)))
-    .join(", ");
-  return `{ ${fieldPropertyListTypes} }`;
+  const operationRootType = graphql.getOperationRootType(schema, operationDef);
+  return genResultType_Selections(
+    schema,
+    operationRootType,
+    operationRootType.getFields(),
+    selectionSet,
+  );
+};
+
+const genResultType_Selections = (
+  schema: graphql.GraphQLSchema,
+  parentType: graphql.GraphQLObjectType | graphql.GraphQLInterfaceType | graphql.GraphQLUnionType,
+  fieldMap: graphql.GraphQLFieldMap<unknown, unknown>,
+  selectionSet: graphql.SelectionSetNode,
+): string => {
+  const fieldPropertyListTypes = selectionSet.selections.flatMap(
+    genResultType_Selection(schema, parentType, fieldMap),
+  );
+  return emitObjectType(fieldPropertyListTypes);
 };
 
 const genResultType_Selection =
-  (schema: graphql.GraphQLSchema, parentType: graphql.GraphQLObjectType) =>
-  (selection: graphql.SelectionNode): string => {
+  (
+    schema: graphql.GraphQLSchema,
+    parentType: graphql.GraphQLObjectType | graphql.GraphQLInterfaceType | graphql.GraphQLUnionType,
+    parentFieldMap: graphql.GraphQLFieldMap<unknown, unknown>,
+  ) =>
+  (selection: graphql.SelectionNode): UnionOfFields => {
     switch (selection.kind) {
       case "Field": {
         const fieldNode: graphql.FieldNode = selection;
 
         if (fieldNode.name.value === "__typename") {
-          // TODO: not sure how this holds on union types and others.
-          return `${fieldNode.name.value}: '${parentType.name}'`;
+          return [
+            { propertyFields: [{ name: fieldNode.name.value, type: `'${parentType.name}'` }] },
+          ];
         } else {
-          const parentFieldMap: graphql.GraphQLFieldMap<any, any> = parentType.getFields();
           const field = parentFieldMap[fieldNode.name.value];
           const fieldType = field.type;
           const namedType: graphql.GraphQLNamedType = graphql.getNamedType(fieldType);
           const typeModifierWrapper = getTypeModifierWrapper(fieldType);
 
           let tsBaseType: string;
-          if (fieldNode.selectionSet) {
-            if (!(namedType instanceof graphql.GraphQLObjectType)) {
-              throw new Error(
-                "genResultType_Selection: Encountered selectionSet on non-object field.",
+          const selectionSet = fieldNode.selectionSet;
+          if (selectionSet) {
+            // TODO: Refactor
+            if (
+              namedType instanceof graphql.GraphQLObjectType ||
+              namedType instanceof graphql.GraphQLInterfaceType
+            ) {
+              tsBaseType = genResultType_Selections(
+                schema,
+                namedType,
+                namedType.getFields(),
+                selectionSet,
+              );
+            } else if (namedType instanceof graphql.GraphQLUnionType) {
+              const unionType: graphql.GraphQLUnionType = namedType;
+              tsBaseType = genResultType_Selections(
+                schema,
+                unionType,
+                parentFieldMap,
+                selectionSet,
               );
             } else {
-              tsBaseType = `{${fieldNode.selectionSet.selections
-                .map(genResultType_Selection(schema, namedType))
-                .join(", ")}}`;
+              throw new Error(
+                `genResultType_Selection: Encountered selectionSet at field ${fieldNode.name.value},` +
+                  ` with non-object type ${namedType.name}.`,
+              );
             }
           } else {
             tsBaseType = convertScalars(namedType.name);
           }
-          return `${fieldNode.name.value}: ${typeModifierWrapper(tsBaseType)}`;
+          return [
+            {
+              propertyFields: [
+                {
+                  name: fieldNode.name.value,
+                  type: typeModifierWrapper(tsBaseType),
+                },
+              ],
+            },
+          ];
         }
       }
       case "FragmentSpread": {
+        // Fragments cannot be declared in a schema, so we can't support them currently as only single operations are allowed.
+        // Allowing optional fragments besides the operation would be easy, but not very useful unless there's a way to
+        // share fragments between queries.
         throw new Error("genResultType_Selection: Unsupported SelectionNode: FragmentSpreadNode");
       }
       case "InlineFragment": {
-        throw new Error(
-          "genResultType_SelectiongenResultType_Selection: Unsupported SelectionNode: InlineFragmentNode",
-        );
+        const inlineFragment: graphql.InlineFragmentNode = selection;
+        let conditionOrParentType:
+          | graphql.GraphQLObjectType
+          | graphql.GraphQLInterfaceType
+          | graphql.GraphQLUnionType;
+
+        let fieldMap: graphql.GraphQLFieldMap<unknown, unknown>;
+
+        if (inlineFragment.typeCondition === undefined) {
+          // The type condition is undefined, for `... { FIELDS }` fragments (which are redundant and can be
+          // replaced with just `FIIELDS`).
+          conditionOrParentType = parentType;
+          fieldMap = parentFieldMap;
+        } else {
+          const conditionType = typeFromAst(schema, inlineFragment.typeCondition);
+
+          if (conditionType === undefined) {
+            // Should never happen because of validation.
+            throw new Error("genResultType_Selection: Undefined type condition");
+          }
+          if (!(conditionType instanceof graphql.GraphQLObjectType)) {
+            // TODO: Can we have list & non-null here?
+            throw new Error("genResultType_Selection: Condition type not an object type");
+          }
+          conditionOrParentType = conditionType;
+          fieldMap = conditionType.getFields();
+        }
+        const inlineSelectionSet = inlineFragment.selectionSet;
+
+        return graphql.isTypeSubTypeOf(schema, conditionOrParentType, parentType)
+          ? inlineSelectionSet.selections
+              .flatMap(genResultType_Selection(schema, conditionOrParentType, fieldMap))
+              .map(({ propertyFields, condition }) => ({
+                propertyFields,
+                // For fields without conditions, use conditionOrParentType.
+                condition: condition ?? conditionOrParentType,
+              }))
+          : [];
       }
     }
   };
